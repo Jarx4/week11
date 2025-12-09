8952cee46ef00dab7eb6642276eb01b8e0402326
Jarko Eier <jarko.eier@tktk.ee>
2025-12-09 21:53:22 +0200

    week 12

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fullRecipeRoutes = require(`./routes/fullRecipes.routes`)
 const randomRoutes = require(`./routes/randomRecipe.routes`);
 const app = express ();
 
+app.use(express.json()); 
 
 app.use('/ingredients', ingredientRouter);
 app.use('/recipes', recipeRouter);
